d8214a79570c9a420b082e30a7ef6465ad169f05
Shahid ansari <72285021+Shahid-coder@users.noreply.github.com>
2021-05-03 12:10:35 +0530

    Update emailbot.py

--- emailbot.py
+++ emailbot.py
@@ -6,28 +6,12 @@ server=smtplib.SMTP("smtp.gmail.com",587)
 
 server.starttls()#for starting tls
 
-server.login("your email","password")
-
-i=0
-
-while i<10: #no of emails you want to send
-
-    i+=1
-
-    server.sendmail("from","to","message")'''This is a simple email bot in which you can sent no. Of automatic  emails you want using python smtplib'''
-
-import smtplib #built in modules
-
-server=smtplib.SMTP("smtp.gmail.com",587)
-
-server.starttls()#for starting tls
-
 server.login("your-email","your-password")
 
 i=0
 
-while i<=10: #no of emails you want to send
+while i<no_of_email: #no of emails you want to send
 
     i+=1
 
-    server.sendmail("from","to","message")
+    server.sendmail("from","to","message")'''This is a simple email bot in which you can sent no. Of automatic  emails you want using python smtplib'''
